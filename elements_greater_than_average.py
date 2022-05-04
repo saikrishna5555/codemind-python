9e932a026c2a636ef97a3f4bbbf2428031bd09fb
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-05-04 21:33:18 +0530

    def avg(d):     n=len(d)     a=sum(d)     avg=0     c=0     avg=a//n     for i in range(n):         if d[i]>=avg:             c+=1     return c n=int(input()) d=list(map(int,input().split())) res=avg(d) print(res)

---
+++ elements_greater_than_average.py
@@ -0,0 +1,14 @@
+def avg(d):
+    n=len(d)
+    a=sum(d)
+    avg=0
+    c=0
+    avg=a//n
+    for i in range(n):
+        if d[i]>=avg:
+            c+=1
+    return c
+n=int(input())
+d=list(map(int,input().split()))
+res=avg(d)
+print(res)
