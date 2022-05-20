b5e98932c3e37deecaa50c828231a322dd961768
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-05-20 20:38:23 +0530

    def vowels(d):     c=0     for i in d:         if i in d1:             c+=1     return c
    n=input()
    v='aeiouAEIOU'
    d1=list(v)
    s=n.split()
    d=list(s)
    a=[]
    for i in d:
        if vowels(i):
            a.append(vowels(i))
    print(max(a))

---
+++ maximum_vowel_count.py
@@ -0,0 +1,19 @@
+def vowels(d):
+    c=0
+    for i in d:
+        if i in d1:
+            c+=1
+    return c
+
+
+
+n=input()
+v='aeiouAEIOU'
+d1=list(v)
+s=n.split()
+d=list(s)
+a=[]
+for i in d:
+    if vowels(i):
+        a.append(vowels(i))
+print(max(a))
