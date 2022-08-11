f583dbf012eaa60303a480286ece3f428e0ae3f5
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-08-11 22:10:14 +0530

    t=int(input()) for i in range(t):     a=int(input())     i=0     d=0     res=0     while a:         d=a%10         a=a//10         res+=d*(2**i)         i+=1     print(res)

---
+++ Binary_to_Decimal.py
@@ -0,0 +1,12 @@
+t=int(input())
+for i in range(t):
+    a=int(input())
+    i=0
+    d=0
+    res=0
+    while a:
+        d=a%10
+        a=a//10
+        res+=d*(2**i)
+        i+=1
+    print(res)
