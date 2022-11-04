a93e3d74e1230429d5392b6c13959d7772799941
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-11-04 11:56:08 +0530

    n=int(input()) s='123456789' for i in range(1,n+1):     print(s[0:i])

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,4 @@
 n=int(input())
-dic={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H',9:'I',10:'J',11:'K',12:'L',13:'M',14:'N',15:'O',16:'P',17:'Q',18:'R',19:'S',20:'T',21:'U',22:'V',23:'W',24:'X',25:'Y',26:'Z'}
-t=n
-for i in range(1,t+1):
-    for j in range(n):
-        print(dic[n],end=' ')
-    n-=1
-    print()
+s='123456789'
+for i in range(1,n+1):
+    print(s[0:i])
