6c1e102f3b8b7089ef21945a78fb1a6cb7a92c2f
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-11-05 09:10:34 +0530

    n,m=map(int,input().split()) e=[] for i in range(n):     d=list(map(int,input().split()))     e.append(d) s=0 e1=[] for i in range(m):     for j in range(n):         s+=e[j][i]     e1.append(s)     s=0 print(max(e1))

---
+++ Maximum_sum_of_column_wise_sum_of_a_matrix.py
@@ -0,0 +1,13 @@
+n,m=map(int,input().split())
+e=[]
+for i in range(n):
+    d=list(map(int,input().split()))
+    e.append(d)
+s=0
+e1=[]
+for i in range(m):
+    for j in range(n):
+        s+=e[j][i]
+    e1.append(s)
+    s=0
+print(max(e1))
